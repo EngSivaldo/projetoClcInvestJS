4bad5788756d097d377a1b321586af5a8a923baf
EngSivaldo <sivaldovieiradealmeida@gmail.com>
2024-11-06 12:37:36 -0300

    Update index.js
    mudanca feito no terminal do github

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import {turboLogger} from './turboLogger.js';
 
 console.log("testando o github");
 turboLogger("estou aprendendo o curso do git", "red");
+turboLogger("estou aprendendo o curso do git", "yellow");
