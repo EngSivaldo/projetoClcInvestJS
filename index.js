661ec3dc9b5c01229e85c261fc46cb7b155bfc81
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-11-06 12:03:16 -0300

    atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import {turboLogger} from './turboLogger.js';
 
 console.log("testando o github");
 turboLogger("estou aprendendo o curso do git", "red");
+turboLogger("Eu consegui criar uma nova mensagem sem afetar a versao principal do sistema!", "darkgreen");
